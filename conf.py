967acb8b90543768024d1821da40b3ea21a7645f
hp-printerr <106080415+hp-printerr@users.noreply.github.com>
2025-06-30 19:21:43 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'HP Smart'
+project = 'Brother Printer Driver'
 
 
 # The full version, including alpha/beta/rc tags
